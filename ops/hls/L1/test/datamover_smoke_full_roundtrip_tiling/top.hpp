b405b7ec481fc1c510d8f1999359292bffe1358d
Beniel Thileepan <thileepanbeniel@gmail.com>
2025-11-28 03:31:02 +0300

    OPS HLS L1 TEST datamover_smoke_full_roundtrip_tilling

---
+++ ops/hls/L1/test/datamover_smoke_full_roundtrip_tiling/top.hpp
@@ -0,0 +1,29 @@
+
+// #define DEBUG_LOG
+#include "../../../include/device/ops_hls_kernel_support.h"
+
+#define MEM_VECTOR_SIZE 4
+#define AXI_M_WIDTH 128
+#define AXIS_WIDTH 64
+#define HLS_STREAM_WIDTH 128
+
+//template <unsigned int MEM_DATA_WIDTH, unsigned int STREAM_DATA_WIDTH, unsigned int BURST_SIZE=32>
+void dut(ap_uint<AXI_M_WIDTH>* mem_in_b1,
+        ap_uint<AXI_M_WIDTH>* mem_in_b2,
+		ap_uint<AXI_M_WIDTH>* mem_out_b1,
+        ap_uint<AXI_M_WIDTH>* mem_out_b2,
+        const unsigned short dim,
+        const unsigned short grid_x_size,
+        const unsigned short grid_y_size,
+        const unsigned short grid_z_size,
+        const unsigned short range_start_0,
+        const unsigned short range_end_0,
+        const unsigned short range_start_1,
+        const unsigned short range_end_1,
+        const unsigned short range_start_2,
+        const unsigned short range_end_2,
+        const unsigned short tile_size_x,
+        const unsigned short tile_size_y,
+        const unsigned short overlap_size_x,
+        const unsigned short overlap_size_y);
+
