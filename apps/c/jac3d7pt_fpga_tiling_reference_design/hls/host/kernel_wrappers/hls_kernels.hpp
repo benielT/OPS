7aff6e41b9f6acf59b5a0efed10d18d88fd66e1b
Beniel Thileepan <Beniel.Thileepan@warwick.ac.uk>
2026-01-02 23:30:38 +0100

    OPS HLS tiling reference design app jac3d added with init changes

---
+++ apps/c/jac3d7pt_fpga_tiling_reference_design/hls/host/kernel_wrappers/hls_kernels.hpp
@@ -0,0 +1,22 @@
+// Auto-generated at 2025-09-06 21:18:49.555818 by ops-translator
+
+#pragma once
+
+#include <math.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#include <ops_hls_rt_support.h>
+
+typedef float stencil_type;
+constexpr unsigned short vector_factor = 8;
+constexpr unsigned short mem_vector_factor = 16;
+constexpr unsigned int iter_par_factor = 9;
+
+// user kernel files
+#include "kernel_populate_kernel.hpp"
+#include "kernel_initialguess_kernel.hpp"
+#include "kernel_copy_kernel.hpp"
+#include "outerloop_0.hpp"
+
