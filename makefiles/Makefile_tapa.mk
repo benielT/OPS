a2842d1059b10123a57efa62d3b0b33394f58b1a
Beniel Thileepan <Beniel.Thileepan@warwick.ac.uk>
2026-07-09 12:34:26 +0200

    OPS TAPA makefile module initial commit

--- makefiles/Makefile_tapa.mk
+++ makefiles/Makefile_tapa.mk
@@ -1,27 +1,75 @@
 # Makefile_tapa.mk
 
 # Checks for required Xilinx tools
+XSA_PLATFORMS := vck5000 v80
+
+# Checks for XILINX_VITIS
 ifndef XILINX_VITIS
-$(error XILINX_VITIS variable is not set. Please source the Vitis settings64.sh.)
+$(error XILINX_VITIS variable is not set. Please set it using "source <Vitis_install_path>/Vitis/<Version>/settings64.sh" and rerun.)
 endif
 
+# Checks for XILINX_VITIS
+ifndef XILINX_HLS
+$(error XILINX_HLS variable is not set. Please set it using "source <Vitis_install_path>/Vitis/<Version>/settings64.sh" and rerun.)
+endif
+
+# Checks for XILINX_XRT
 ifndef XILINX_XRT
-$(error XILINX_XRT variable is not set. Please source the XRT setup.sh.)
+$(error XILINX_XRT variable is not set. Please set it using "source /opt/xilinx/xrt/setup.sh" and rerun.)
+endif
+
+# Check PLATFORM 
+ifeq ($(PLATFORM),)
+ifneq ($(DEVICE),)
+$(warning WARNING: DEVICE is deprecated in make command. Please use PLATFORM instead.)
+PLATFORM := $(DEVICE)
+else
+$(error PLATFORM not set. Please set the PLATFORM properly and rerun.)
+endif
+else
+$(info hls platform: $(PLATFORM))
+endif
+
+# define OPS_HLS_XSA_PLATFORM if PLATFORM is in XSA_PLATFORMS
+$(foreach P,$(XSA_PLATFORMS), \
+	$(if $(findstring $(P),$(PLATFORM)), \
+		$(eval OPS_HLS_XSA_PLATFORM := 1) \
+	) \
+)
+
+ifdef OPS_HLS_XSA_PLATFORM
+$(info Found XSA platform)
+else
+$(info Found non XSA platform)
 endif
 
 # ---------------------------------------------------------
 # TAPA Submodule Configuration
 # ---------------------------------------------------------
-TAPA_BASE_DIR = $(OPS_INSTALL_PATH)/../tapa
-TAPA_INC_DIR  = $(TAPA_BASE_DIR)/src
+TAPA_BASE_DIR = $(OPS_INSTALL_PATH)/hls
+TAPA_INC_DIR  = $(TAPA_BASE_DIR)/include
 TAPA_LIB_DIR  = $(TAPA_BASE_DIR)/build
+
+
+#check tapa installed or not
+ifeq ($(TAPA_INSTALL_DIR),)
+$(error TAPA_INSTALL_DIR not set. Please set the TAPA_INSTALL_DIR properly and rerun.)
+endif 
+
+TAPA_INCLUDE_DIR=$(TAPA_INSTALL_DIR)/usr/include
+TAPA_LIBRARY_DIR=$(TAPA_INSTALL_DIR)/usr/lib
 
 # TAPAC compiler location. 
 # Defaults to the system PATH, but can be overridden if executing from a local virtualenv or build directory.
-TAPAC ?= tapac
+TAPAC ?= tapacc
 
 ifeq (, $(shell which $(TAPAC)))
-$(warning WARNING: $(TAPAC) not found in PATH. Ensure the TAPA submodule has been built and the binary is accessible.)
+$(error ERROR: $(TAPAC) not found in PATH. Ensure the TAPA submodule has been built and the binary is accessible.)
+endif
+
+TAPA ?= tapa
+ifeq (, $(shell which $(TAPA)))
+$(error  ERROR: $(TAPA) not found in PATH. Ensure the TAPA submodule has been built and the binary is accessible.)
 endif
 
 # ---------------------------------------------------------
@@ -29,25 +77,40 @@ endif
 # ---------------------------------------------------------
 ifeq ($(PLATFORM),)
 $(error PLATFORM not set. Please set the PLATFORM properly and rerun.)
+else ifeq ($(PLATFORM_PATH),)
+$(error PLATFORM_PATH not set. Please set the PLATFORM_PATH properly and rerun.)
 endif
 
 ifeq ($(HLS_JOBS),)
 HLS_JOBS = 10
 endif
 
-ifeq ($(HLS_TARGET_MODE),)
+ifeq ($(HLS_TARGET_MODE),sw_emu)
+$(info hls mode: SW_EMU)
+else ifeq ($(HLS_TARGET_MODE),hw_emu)
+$(info hls mode: HW_EMU)
+else ifeq ($(HLS_TARGET_MODE),hw)
+$(info hls mode: HW)
+else
 $(error hls build mode is not set: please set HLS_TARGET_MODE=<sw_emu/hw_emu/hw>)
+endif
+
+ifeq ($(TAPA_CLOCK_PERIOD),)
+TAPA_CLOCK_PERIOD = 3.33
+endif 
+
+ifeq ($(TAPA_WORK_DIR),)
+TAPA_WORK_DIR = ./hls/build/$(HLS_TARGET_MODE)/tapa_work_dir
 endif
 
 $(info TAPA target mode: $(HLS_TARGET_MODE) on $(PLATFORM))
 
 # Vitis and Workspace Variables
 VPP = v++
-TAPA_WORK_DIR = ./tapa/build/$(HLS_TARGET_MODE)/tapa_work_dir
 
 # OPS Include Paths
-OPS_HLS_DEVICE_INC = -I$(OPS_INSTALL_PATH)/hls/include/device/ -I$(OPS_INSTALL_PATH)/hls/L1/include/
-OPS_HLS_HOST_INC   = -I$(OPS_INSTALL_PATH)/c/include/ -I$(OPS_INSTALL_PATH)/hls/include/host/ -I$(OPS_INSTALL_PATH)/hls/ext/xcl2/ -I$(OPS_INSTALL_PATH)/hls/L1/include/ -I$(OPS_INSTALL_PATH)/hls/L2/include/
+TAPA_HLS_DEVICE_INC = -I$(OPS_INSTALL_PATH)/hls/include/device/ -I$(OPS_INSTALL_PATH)/hls/L1/include/
+TAPA_HLS_HOST_INC   = -I$(OPS_INSTALL_PATH)/c/include/ -I$(OPS_INSTALL_PATH)/hls/include/host/ -I$(OPS_INSTALL_PATH)/hls/ext/xcl2/ -I$(OPS_INSTALL_PATH)/hls/L1/include/ -I$(OPS_INSTALL_PATH)/hls/L2/include/
 
 # ---------------------------------------------------------
 # Compilation & Linker Flags
@@ -55,13 +118,13 @@ OPS_HLS_HOST_INC   = -I$(OPS_INSTALL_PATH)/c/include/ -I$(OPS_INSTALL_PATH)/hls/
 
 # TAPA Device Compilation Flags
 # Note: The tapac command requires standard include directives to locate the submodule's tapac.h
-TAPA_FLAGS = --platform $(PLATFORM) --work-dir $(TAPA_WORK_DIR) --clock-period 3.33 
-
+TAPA_FLAGS = --platform $(PLATFORM_PATH)/$(PLATFORM) --clock-period $(TAPA_CLOCK_PERIOD)
 # Vitis Linking Flags for .xo to .xclbin phase
-VPP_LINK_FLAGS = -t $(HLS_TARGET_MODE) --platform $(PLATFORM) --jobs $(HLS_JOBS)
+VPP_LINK_FLAGS= --target $(HLS_TARGET_MODE) --platform $(PLATFORM) --hls.jobs $(HLS_JOBS) --remote_ip_cache $(HLS_IP_CACHE_DIR)
 
 # Host Executable Flags (Injects the submodule include paths)
-TAPA_CXXFLAGS = -I$(TAPA_INC_DIR) -I$(XILINX_XRT)/include/ -I$(XILINX_VIVADO)/include/ -DVITIS_PLATFORM=$(PLATFORM) -DOPS_FPGA -D__USE_XOPEN2K8 -fmessage-length=0 $(OPS_HLS_HOST_INC) -D__VITIS_HLS__
+TAPA_CXXFLAGS = -I$(TAPA_INC_DIR) -I$(XILINX_XRT)/include/ -I$(XILINX_VIVADO)/include/ -I$(TAPA_INCLUDE_DIR) -DVITIS_PLATFORM=$(PLATFORM) -DOPS_FPGA -D__USE_XOPEN2K8 -I$(XILINX_HLS)/include/ -fmessage-length=0 $(TAPA_HLS_HOST_INC) -D__VITIS_HLS__
+
 
 # Host Linker Flags (Injects the submodule library path for libtapa)
-TAPA_LDFLAGS = -L$(TAPA_LIB_DIR) -ltapa -lgflags -lglog -lxilinxopencl -lops_seq -lops_hls -lpthread -lrt -lstdc++ -L$(XILINX_XRT)/lib/ -L$(OPS_INSTALL_PATH)/hls/lib/$(OPS_COMPILER)/ -Wl,-rpath-link,$(XILINX_XRT)/lib
+TAPA_LDFLAGS = -L$(TAPA_LIB_DIR) -lxilinxopencl -lops_seq -lops_hls -lpthread -lrt -lstdc++ -L$(XILINX_XRT)/lib/ -L$(OPS_INSTALL_PATH)/hls/lib/$(OPS_COMPILER)/ -L$(TAPA_LIBRARY_DIR) -Wl,-rpath-link,$(XILINX_XRT)/lib
