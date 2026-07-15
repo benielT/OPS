ba0042089ab32a4c0d4a7db7dd44dcd46f6da7ae
Beniel Thileepan <Beniel.Thileepan@warwick.ac.uk>
2026-07-16 00:44:06 +0200

    OPS TAPA host top include file added

---
+++ ops/hls/include/host/ops_tapa_rt_support.h
@@ -0,0 +1,7 @@
+#include <tapa.h>
+
+#include <ap_int.h>
+#include "../../common/include/ops_hls_defs.hpp"
+#include "../../common/include/ops_hls_utils.hpp"
+#include "../../common/include/ops_hls_common_memconfig.hpp"
+#include "../../L1/include/ops_tapa_datamover.hpp"
